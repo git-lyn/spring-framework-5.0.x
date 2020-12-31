d9e95072832c7adfbb3407e780d579531381bd3b
lyn-git <1415883718@qq.com>
2020-12-31 14:56:03 +0800

    test22 branch

--- luban-pro/src/main/java/com/luban/test/BB.java
+++ luban-pro/src/main/java/com/luban/test/BB.java
@@ -9,6 +9,7 @@ public class BB {
 	public static void main(String[] args) {
 		System.out.println("BBBBBBBBBB");
 		System.out.println("test2");
+		System.out.println("test22");
 		System.out.println("BBBBBBBBBB");
 	}
 }
